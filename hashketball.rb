c5979018bc1980c7686e99c7754ed8bd925143cf
Jacob Kagon <jacob@kagon.net>
2020-09-20 18:12:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,21 @@
 # Write your code here!
+def builidng_hasketball
+  game_hash = {
+    :home => {
+      :team_name => ""
+      :colors => [""]
+      :players => [{
+        :player_name => 
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      }]
+    },
+    :away => {}
+  }
+end
