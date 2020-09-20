78f551f6e87b19f6d7582b21a0f644aca7725f60
Jacob Kagon <jacob@kagon.net>
2020-09-20 18:18:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,8 @@ def builidng_hasketball
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{
+      :players => [
+        {
         :player_name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
@@ -16,8 +17,19 @@ def builidng_hasketball
         :slam_dunks => 1
       },
       {
+        :player_name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7  
+      }, 
+      {
         
-      }    
+      }
           ]
     },
     :away => {}
